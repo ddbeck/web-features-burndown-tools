30bb80eb74204c626ba641f87ed40c779fd3efbb
Daniel D. Beck <daniel@ddbeck.com>
2024-10-14 20:27:25 +0200

    Migrate config for latest eslint

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "no-warning-comments": ["error", { terms: ["xxx"], location: "anywhere" }],
-  },
-  root: true,
-};
