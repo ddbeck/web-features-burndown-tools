ff6a6b99b0f786c27d0eee60646f9f8dd4332a5f
Daniel D. Beck <daniel@ddbeck.com>
2024-10-14 20:27:59 +0200

    Format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,27 +8,33 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-        },
-
-        rules: {
-            "no-warning-comments": ["error", {
-                terms: ["xxx"],
-                location: "anywhere",
-            }],
-        },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
     },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      "no-warning-comments": [
+        "error",
+        {
+          terms: ["xxx"],
+          location: "anywhere",
+        },
+      ],
+    },
+  },
 ];
